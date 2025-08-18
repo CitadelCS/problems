db89a32dd94ac02b2a1acc123b7b8ed5b9a719ff
mverdicchio <verdicchio@gmail.com>
2025-08-18 15:02:24 -0400

    checks for metric.py

---
+++ metric/__init__.py
@@ -0,0 +1,21 @@
+import check50
+
+@check50.check()
+def exists():
+    """indoor.py exists"""
+    check50.exists("indoor.py")
+    
+@check50.check()
+def whole_number():
+    """whole number"""
+    check50.run("python3 metric.py").stdin("123", prompt=False).stdout("123.0 feet is 37.49 meters\n123.0 meters is 403.54 feet\n", regex=False).exit(0)
+
+@check50.check()
+def exact_conversion_factor():
+    """exact conversion factor"""
+    check50.run("python3 metric.py").stdin("3048", prompt=False).stdout("3048.0 feet is 929.03 meters\n3048.0 meters is 10000.00 feet\n", regex=False).exit(0)
+
+@check50.check()
+def fractional_number():
+    """fractional number"""
+    check50.run("python3 metric.py").stdin("123.456", prompt=False).stdout("123.456 feet is 37.63 meters\n123.456 meters is 405.04 feet\n", regex=False).exit(0)
